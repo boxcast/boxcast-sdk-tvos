f43925501cc0821b7074b14e5625aa32b825d47b
Mike Griffith <bikegriffith@gmail.com>
2017-05-11 20:20:21 -0400

    fix typo. reformat tabs to spaces.

--- src/BoxCastPlayer.js
+++ src/BoxCastPlayer.js
@@ -9,29 +9,29 @@ import BoxCastPlayerMetrics from './BoxCastPlayerMetrics';
 import BoxCastData from './BoxCastData';
 
 export default class BoxCastPlayer {
-	constructor(broadcast, options={}) {
-		this._broadcast = broadcast;
-		this._options = options;
-		this._api = new BoxCastData(options);
-	}
+  constructor(broadcast, options={}) {
+    this._broadcast = broadcast;
+    this._options = options;
+    this._api = new BoxCastData(options);
+  }
 
-	present() {
-		return this._api.getBroadcastView(this._broadcast.id).then((view) => {
-			if (!view.playlist) {
-				throw `This broadcast is unavailable for viewing. The status is ${view.status}`;
-			}
-		    var player = new Player(),
-		        playlist = new Playlist(),
-		        mediaItem = new MediaItem("video", view.playlist);
-		    player.playlist = playlist;
-		    player.playlist.push(mediaItem);
+  present() {
+    return this._api.getBroadcastView(this._broadcast.id).then((view) => {
+      if (!view.playlist) {
+        throw `This broadcast is unavailable for viewing. The status is ${view.status}`;
+      }
+      var player = new Player(),
+          playlist = new Playlist(),
+          mediaItem = new MediaItem("video", view.playlist);
+      player.playlist = playlist;
+      player.playlist.push(mediaItem);
 
-		    var metrics = new BoxCastPlayerMetrics(broadcast, view, this._options);
-		    metrics.attach(player);
+      var metrics = new BoxCastPlayerMetrics(this._broadcast, view, this._options);
+      metrics.attach(player);
 
-		    player.present();
+      player.present();
 
-		    return player;
-		});
-	}
+      return player;
+    });
+  }
 }
